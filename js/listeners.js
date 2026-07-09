51d74a998d233abe7e60900982d861e3db52f27f
SLY-NSX <1010593711@qq.com>
2026-07-09 15:50:50 +0800

    Add files via upload

--- js/listeners.js
+++ js/listeners.js
@@ -3199,7 +3199,6 @@ window.toggleCollapsedExtras = function() {
             extra.addEventListener('click', (e) => { e.stopPropagation(); primary.click(); });
         }
     }
-    wireExtra('combo-btn-extra', 'combo-btn');
     wireExtra('batch-btn-extra', 'batch-btn');
 };
 
